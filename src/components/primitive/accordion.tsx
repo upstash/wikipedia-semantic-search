15e2d41c49e66d63adcbdc06f2d407d78709783a
ytkimirti <yusuftaha9@gmail.com>
2024-08-15 13:16:44 +0300

    fix accordion shaking when opening because of padding

--- src/components/primitive/accordion.tsx
+++ src/components/primitive/accordion.tsx
@@ -50,10 +50,10 @@ const AccordionContent = React.forwardRef<
 >(({ className, children, ...props }, ref) => (
   <AccordionPrimitive.Content
     ref={ref}
-    className="overflow-hidden p-4 text-sm transition-all data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down"
+    className="overflow-hidden text-sm transition-all data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down"
     {...props}
   >
-    <div className={cn("pb-4 pt-0", className)}>{children}</div>
+    <div className={cn("pb-4 p-4", className)}>{children}</div>
   </AccordionPrimitive.Content>
 ));
 
