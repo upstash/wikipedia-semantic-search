48c1555d24376a1ce5b9ef3ceb6faa6bb07e5d08
ytkimirti <yusuftaha9@gmail.com>
2024-08-23 14:49:42 +0300

    fix error when unkown locale given

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,11 +1,29 @@
 import { getRequestConfig } from "next-intl/server";
 import { getUserLocale } from "./service";
 
+const supportedLanguages = [
+  "de",
+  "en",
+  "es",
+  "fa",
+  "fr",
+  "it",
+  "ja",
+  "pt",
+  "ru",
+  "tr",
+  "zn",
+];
+
 export default getRequestConfig(async () => {
-  const locale = await getUserLocale();
+  const userLocale = await getUserLocale();
+
+  const locale = supportedLanguages.includes(userLocale) ? userLocale : "en";
+
+  console.log(`User locale: ${locale}`);
 
   return {
-    locale,
+    locale: locale,
     messages: (await import(`../messages/${locale}.json`)).default,
   };
 });
