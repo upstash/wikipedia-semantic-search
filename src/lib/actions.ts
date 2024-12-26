cb1761f731f2b57b06c949662bb3d2360c1582de
CahidArda <cahidardaooz@hotmail.com>
2024-12-26 13:11:42 +0300

    fix: rm keyword selection

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -7,6 +7,7 @@ import { Info, ResultCode, WikiMetadata } from "@/lib/types";
 import { index } from "./dbs";
 import { MessageMetadata } from "./message-meta";
 import { ragChat } from "./rag-chat";
+import { type Index } from "@upstash/vector";
 
 export async function serverGetMessages() {
   const sessionId = cookies().get("sessionId")?.value;
@@ -29,51 +30,8 @@ export async function serverClearMessages() {
   await ragChat.history.deleteMessages({ sessionId });
 }
 
-const capitalizeWord = (word: string) => {
-  return word.charAt(0).toUpperCase() + word.slice(1);
-};
-
-async function getKeywords(query: string) {
-  const resp = await upstash("meta-llama/Meta-Llama-3-8B-Instruct", {
-    analytics: {
-      name: "helicone",
-      token: process.env.HELICONE_TOKEN!,
-    },
-  }).invoke(`
-    Please provide a list of keywords about the question given in JSON format.
-    Don't answer with anything else.
-
-    EXAMPLE INPUT:
-    Ghandi
-
-    EXAMPLE OUTPUT:
-    ["Ghandi", "India", "peace", "leader", "non-violence", "freedom"]
-
-    INPUT:
-    ${query.split(" ").map(capitalizeWord).join(" ")}
-
-    OUTPUT:
-    `);
-
-  console.log(resp);
-
-  try {
-    // @ts-ignore
-    return JSON.parse(resp.content) as string[];
-  } catch (error) {
-    console.error("Error parsing keywords, prompt:", resp.content);
-    return undefined;
-  }
-}
-
 export async function serverQueryIndex(query: string) {
   try {
-    const keywords = await getKeywords(query);
-    console.log("query: ", query, "keywords: ", keywords);
-
-    if (keywords && keywords.length > 0)
-      query = query + " " + keywords.join(" ");
-
     const parsedCredentials = z
       .object({
         query: z.string().min(2),
@@ -90,7 +48,7 @@ export async function serverQueryIndex(query: string) {
       };
     }
 
-    const q = {
+    const q: Parameters<Index["query"]>[0] = {
       data: query as string,
       topK: 100,
       includeData: true,
